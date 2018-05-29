ccd36237720f340ff67dea73a4a55262e50be6b3
liuzixiaosz <995255189@qq.com>
2018-05-29 23:20:03 +0800

    Db connection and change name by id completed

--- src/Client.java
+++ src/Client.java
@@ -5,7 +5,7 @@ import dao.DbConnection;
 public class Client {
 
     public static void main(String[] args) {
-        DbConnection.setDbUrl("/Users/num9527/IdeaProjects/Inflight-Entertainment-System/dbres/In-flight_Entertainer.db");
+       // DbConnection.setDbUrl("/Users/num9527/IdeaProjects/Inflight-Entertainment-System/dbres/In-flight_Entertainer.db");
         CustomUtils.addCustom(() -> {
             CustomUtils.addMovieType("Action", "Comedy", "Horror", "Others");
             CustomUtils.changeTypeNameById("type1", "Documentary");
