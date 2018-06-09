a5d519e4c0a31c18df9d8f053d234c472f26073d
l0uvre <bladwinw@gmail.com>
2018-06-09 11:08:15 +0800

    add some javadocs.

--- src/utils/FilmInfoScrap.java
+++ src/utils/FilmInfoScrap.java
@@ -12,6 +12,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+/***
+ * A class utility which implements Runnable interface, is able to scrap several films' posters, basic
+ * information and corresponding IMDb links concurrently with the help of JSoup library.
+ */
 public class FilmInfoScrap implements Runnable {
   private String filmName;
   private Document doc; // Wikipedia page for the movie to scrap
@@ -23,14 +27,22 @@ public class FilmInfoScrap implements Runnable {
   private static String datafile =  basePath + fs.getSeparator()
           + "data" + fs.getSeparator() + "data.txt";
 
+  /***
+   * Constuctor for scrapping a film's info on corresponding wikipedia page.
+   * @param filmName The English name for the specific film, and a only well-formed name is accepted.
+   * @throws IOException It will use Jsoup underlying to get the page's doc.
+   */
   public FilmInfoScrap(String filmName) throws IOException {
     this.filmName = filmName;
     this.doc = Jsoup.connect("https://en.wikipedia.org/wiki/" + filmName).get(); //If other language needed, can change the wikipedia page to other urls.
 
   }
 
-
-
+  /***
+   * The method will return a formed string which is separated by '|' and fields are film name, director, cast, and
+   * a plot summary.
+   * @return the string described above.
+   */
   public String scrapInfo() {
     String line = new String(filmName);
 
@@ -77,6 +89,10 @@ public class FilmInfoScrap implements Runnable {
     return line;
   }
 
+  /***
+   * This method will get the film's IMDb link, which may be very helpful for more concrete information.
+   * @return A represented String.
+   */
   public String getIMDbLink() {
     Elements links = doc.select("a.external.text");
     // System.out.println(links.size());
@@ -90,10 +106,19 @@ public class FilmInfoScrap implements Runnable {
     return imdbLink;
   }
 
+  /***
+   * A method to return the film's post url resource.
+   * @return the post url
+   */
   public String getImgLink() {
     return imgLink;
   }
 
+  /***
+   * Download the film's poster into the data directory.
+   * @return 0 if succeeded; -1 if failed.
+   * *
+   */
   public int writeImg() {
     /*String basePath = Paths.get(".").toAbsolutePath().normalize().toString();
     FileSystem fs = FileSystems.getDefault();*/
@@ -111,6 +136,10 @@ public class FilmInfoScrap implements Runnable {
     return -1;
   }
 
+  /***
+   *  Save the film's basic information into a local file, allowing database usage.
+   * @return 0 if succeeded; -1 if failed.
+   */
   public int writeInfo() {
     FileWriter fw = null;
     synchronized (datafile) {
@@ -129,6 +158,10 @@ public class FilmInfoScrap implements Runnable {
     return -1;
 
   }
+
+  /***
+   * Override the run() method, which means the entry of a thread.
+   */
   @Override
   public void run() {
     scrapInfo();
